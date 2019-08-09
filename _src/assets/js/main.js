cf4b37bc7e19c3adcea592c803920306579dcff6
btdvalle <btdvalle@gmail.com>
2019-08-09 15:27:52 +0200

    Add collapsible

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -179,6 +179,26 @@ const applyXbtn = function() {
   printFavorites();
 };
 
+//collapsible
+const collapsibleTriggers = document.querySelectorAll(".js-collapsible-trigger");
+
+function updateCollapsible(event) {
+  const currentCollapsible = event.currentTarget.parentElement;
+
+  if (currentCollapsible.classList.contains("js-collapsible-open")) {
+    currentCollapsible.classList.remove("js-collapsible-open");
+  } else {
+    for (const item of collapsibleTriggers) {
+      item.parentElement.classList.remove("js-collapsible-open");
+    }
+    currentCollapsible.classList.add("js-collapsible-open");
+  }
+}
+
+for (const item of collapsibleTriggers) {
+  item.addEventListener("click", updateCollapsible);
+}
+
 
 //ejecutar funciones
 btn.addEventListener("click", searchInServer);
